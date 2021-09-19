982ed2a7681eb28b6a037dc328b4ee7945acfb21
Jack Middlebrook <jack.middlebrook@gmail.com>
2021-09-19 18:25:18 +0800

    Add html

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -58,12 +58,31 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
+  def route(%{method: "GET", path: "/about"} = conv) do
+    Path.expand("../../pages", __DIR__)
+      |> Path.join("about.html")
+      |> File.read
+      |> handle_file(conv)
+  end
+
   def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
     %{conv | status: 403, resp_body: "Please don't delete Bear #{id}. Thank you"}
   end
 
   def route(conv) do
     %{conv | status: 404, resp_body: "No #{conv.path} here!"}
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 500, resp_body: "File not found!" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
   end
 
   def format_response(conv) do
@@ -166,6 +185,18 @@ IO.puts(response)
 
 request = """
 GET /wrong HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
