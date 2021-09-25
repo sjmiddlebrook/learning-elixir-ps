e89226438ce9a3aa79fe2e5a95d436ac478fe2f4
Jack Middlebrook <jack.middlebrook@gmail.com>
2021-09-25 15:18:20 +0800

    Add separate json file with bears and parse

--- lib/servy/wildthings.ex
+++ lib/servy/wildthings.ex
@@ -2,18 +2,31 @@ defmodule Servy.Wildthings do
   alias Servy.Bear
 
   def list_bears do
-    [
-      %Bear{id: 1, name: "Teddy", type: "Brown", hibernating: true},
-      %Bear{id: 2, name: "Smokey", type: "Black"},
-      %Bear{id: 3, name: "Paddington", type: "Brown"},
-      %Bear{id: 4, name: "Scarface", type: "Grizzly", hibernating: true},
-      %Bear{id: 5, name: "Snow", type: "Polar"},
-      %Bear{id: 6, name: "Brutus", type: "Grizzly"},
-      %Bear{id: 7, name: "Rosie", type: "Black", hibernating: true},
-      %Bear{id: 8, name: "Roscoe", type: "Panda"},
-      %Bear{id: 9, name: "Iceman", type: "Polar", hibernating: true},
-      %Bear{id: 10, name: "Kenai", type: "Grizzly"}
-    ]
+    Path.expand("../../db", __DIR__)
+    |> Path.join("bears.json")
+    |> read_json
+    |> parse_json
+    |> initialize_bears
+  end
+
+  defp initialize_bears(bear_list) do
+    for bear <- bear_list do
+      struct(Bear, bear)
+    end
+  end
+
+  defp parse_json(json) do
+    Jason.decode!(json, keys: :atoms) |> Map.get(:bears)
+  end
+
+  defp read_json(source) do
+    case File.read(source) do
+      {:ok, contents} ->
+        contents
+      {:error, reason} ->
+        IO.inspect "Error reading #{source}: #{reason}"
+        "[]"
+    end
   end
 
   def get_bear(id) when is_integer(id) do
