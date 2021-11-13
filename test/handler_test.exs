920bd8977deb751ccfba1efd91754c447a233f2d
Jack Middlebrook <jack.middlebrook@gmail.com>
2021-11-13 16:33:26 +0800

    Add Httpoison to tests

--- test/handler_test.exs
+++ test/handler_test.exs
@@ -6,6 +6,36 @@ defmodule HandlerTest do
   alias Servy.HttpServer
   alias Servy.HttpClient
 
+  test "accepts a request on a socket and sends back a response" do
+    spawn(HttpServer, :start, [4000])
+
+    parent = self()
+
+    max_concurrent_requests = 5
+
+    # Spawn the client processes
+    for _ <- 1..max_concurrent_requests do
+      spawn(
+        fn ->
+          # Send the request
+          {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+
+          # Send the response back to the parent
+          send(parent, {:ok, response})
+        end
+      )
+    end
+
+    # Await all {:handled, response} messages from spawned processes.
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
+  end
+
   test "Spawn server and get api request" do
     spawn(HttpServer, :start, [4000])
 
