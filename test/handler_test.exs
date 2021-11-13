7fa6ab65cec70e527ab9c56f2c3a19dbe598df0b
Jack Middlebrook <jack.middlebrook@gmail.com>
2021-11-13 17:29:12 +0800

    Update tests

--- test/handler_test.exs
+++ test/handler_test.exs
@@ -6,28 +6,29 @@ defmodule HandlerTest do
   alias Servy.HttpServer
   alias Servy.HttpClient
 
-  test "accepts a request on a socket and sends back a response" do
+  test "different endpoints send back a response" do
     spawn(HttpServer, :start, [4071])
 
-    parent = self()
+    urls = [
+      "http://localhost:4000/wildthings",
+      "http://localhost:4000/bears",
+      "http://localhost:4000/bears/1",
+      "http://localhost:4000/wildlife",
+      "http://localhost:4000/api/bears"
+    ]
 
-    max_concurrent_requests = 5
-
-    url = "http://localhost:4071/wildthings"
-
-    1..max_concurrent_requests
-    |> Enum.map(fn(_) -> Task.async(fn -> HTTPoison.get(url) end) end)
-    |> Enum.map(&Task.await(&1))
-    |> Enum.map(&assert_successful_response(&1))
+    urls
+    |> Enum.map(&Task.async(fn -> HTTPoison.get(&1) end))
+    |> Enum.map(&Task.await/1)
+    |> Enum.map(&assert_successful_response/1)
   end
 
   defp assert_successful_response({:ok, response}) do
     assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
   end
 
   test "Spawn server and get api request" do
-    spawn(HttpServer, :start, [4071])
+    spawn(HttpServer, :start, [4073])
 
     request = """
     GET /api/bears HTTP/1.1\r
